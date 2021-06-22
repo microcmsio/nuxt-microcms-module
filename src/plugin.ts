fe0793bc678eb8ba5ccafab4822deb7018d8a90b
Kazuki Shibata <k.shibata97@gmail.com>
2021-06-22 11:32:24 +0900

    [fix] serialize

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,7 +2,7 @@ import { Plugin } from '@nuxt/types';
 import { createClient } from 'microcms-js-sdk';
 
 const plugin: Plugin = function (_, inject) {
-  const _options = JSON.parse('<%= options %>')
+  const _options = JSON.parse('<%= serialize(options) %>')
   const client = createClient(_options);
 
   inject('microcms', client);
